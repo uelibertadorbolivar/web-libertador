bad0b62e6da5e92d669ab766080c237c919103a2
UE Libertador Bolívar <64515162+uelibertadorbolivar@users.noreply.github.com>
2026-02-09 14:40:24 -0400

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const AppConfig = {
+    apiUrl: "https://script.google.com/macros/s/AKfycbzifkb5Gw-I5gbdJYSpWpX1vY-pnHY5eQWDQ9oYQ1ymNQ3Lkt11H0nLKtlCOzj0SdCVGg/exec",
+    getApiUrl: function() {
+        return this.apiUrl;
+    }
+};
