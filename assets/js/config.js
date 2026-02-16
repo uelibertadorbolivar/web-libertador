50d9ab4cd19782b12d51a7b606bccf8e0bf3b9f3
UE Libertador Bolívar <64515162+uelibertadorbolivar@users.noreply.github.com>
2026-02-16 18:54:37 -0400

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-const AppConfig = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbyIJqlUNG9xiA6N1AlBjgS-upWxXh4nwVWk51lV3q64b2eqaOsLTViMgO7GiWh8SQGaQQ/exec", // Reemplaza con tu URL de implementación
-    getApiUrl: function() { return this.apiUrl; }
-};
