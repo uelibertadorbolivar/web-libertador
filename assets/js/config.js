294eb90937474335a56cea0cc97d2a546b045533
UE Libertador Bolívar <64515162+uelibertadorbolivar@users.noreply.github.com>
2026-02-20 06:31:27 -0400

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+/**
+ * CONFIGURACIÓN DE LA API - SIGAE v3.1
+ * Backend: Google Apps Script
+ */
+const AppConfig = {
+    apiUrl: "https://script.google.com/macros/s/AKfycbyxH4w2XEvYxrFiNuVZX81aXXe6OvYX6K6e6uwN179_tKN2QhV9g2MRfxU9pAHoGnmJTg/exec",
+    
+    getApiUrl: function() {
+        return this.apiUrl;
+    }
+};
